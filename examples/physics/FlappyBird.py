374c3638560b52cef7e8f922f0aae6a545fe7950
Zack <115175938+ZackWilde27@users.noreply.github.com>
2023-07-17 22:23:21 -0400

    Add files via upload

--- examples/physics/FlappyBird.py
+++ examples/physics/FlappyBird.py
@@ -1,22 +1,35 @@
 import z3dpy as zp
 import pygame
 import random as rand
+import time
 
 print("")
 print("Controls:")
 print("E to jump")
 
+delta = 0
+currentTime = time.time()
+
+# Changing Gravity
+zp.gravity = [0, 10, 0]
+
+zp.worldColour = (0.2, 0.2, 0.4)
+
+# Initialize PyGame
 pygame.init()
-# PgScreen(height, width, bgCol, pygame)
-screen = zp.PgScreen(1280, 720, "black", pygame)
+screen = pygame.display.set_mode((1280, 720))
 clock = pygame.time.Clock()
 
-# Create our camera (x, y, z)
-myCamera = zp.Cam(0, 0, -3)
+# Set the internal screen size
+zp.screenSize = (1280, 720)
+
+# Create a camera (x, y, z)
+myCamera = zp.Cam(0, 0, 0)
+
+
+# The Bird
 
 # Use the LoadMesh function to load an OBJ file
-# LoadMesh has optional arguments for x, y, z, and sclx, scly, sclz.
-
 birdBody = zp.LoadMesh("mesh/bird.obj", 0, 0, 0, 0.75, 0.75, 0.75)
 birdBeak = zp.LoadMesh("mesh/beak.obj", 0, 0, 0, 0.75, 0.75, 0.75)
 
@@ -24,14 +37,14 @@ birdBeak = zp.LoadMesh("mesh/beak.obj", 0, 0, 0, 0.75, 0.75, 0.75)
 zp.MeshSetColour(birdBody, [255, 214, 91])
 zp.MeshSetColour(birdBeak, [255, 52, 38])
 
-# Changing Gravity
-zp.gravity = [0, 4, 0]
-
 # Group two meshes with different colours into one Thing
 bird = zp.Thing([birdBody, birdBeak], 0, 0, 5)
 
 # Setting up Physics Body
 zp.ThingSetupPhysics(bird)
+
+
+# The Ground
 
 planeMesh = zp.LoadMesh("mesh/plane.obj")
 
@@ -39,8 +52,10 @@ zp.MeshSetColour(planeMesh, [119, 255, 102])
 
 plane = zp.Thing([planeMesh], 0, 5, 0)
 
-pipeMesh = zp.LoadMesh("mesh/pipe.obj")
 
+# The Pipes
+
+pipeMesh = zp.LoadMesh("mesh/pipe.obj")
 
 # Offset the second pipe upwards
 otherPipeMesh = zp.LoadMesh("mesh/pipe.obj", 0, -11, 0)
@@ -52,28 +67,29 @@ zp.MeshSetColour(otherPipeMesh, [0, 255, 0])
 
 pipe = zp.Thing([pipeMesh, otherPipeMesh], 10, 0, 5)
 
-
-# Pipe Pair
+# Pipe Pair Functions
 def SpawnPipePair(h):
     zp.ThingSetPosX(pipe, 10)
     zp.ThingSetPosY(pipe, h)
 
 SpawnPipePair(-1)
 
-def Reset():
-    zp.ThingSetPos(bird, [0, 0, 5])
-    zp.ThingSetVelocity(bird, [0, 0, 0])
-    SpawnPipePair(-1)
-
-birdVelocity = 0
-
 
 # Adding everything to internal lists
-
 zp.AddThings([bird, pipe])
 # Optional layer argument, which is normally 2 / the third layer
 zp.AddThing(plane, 1)
 
+# Sun light
+theSun = zp.Light_Sun((0, 0.5, 0.5), 2, 1, (255, 255, 255))
+
+# Append all lights to global list
+zp.lights.append(theSun)
+
+# BakeLighting(things, *bExpensive, *lights, *shadowcasters)
+zp.BakeLighting([bird, pipe, plane])
+
+zp.CamSetPos(myCamera, [zp.ThingGetPosX(bird), zp.ThingGetPosY(bird), -3])
 
 # Raster Loop
 while True:
@@ -83,44 +99,50 @@ while True:
             pygame.quit()
             
     screen.fill("#88E5FF")
-    clock.tick(30)
+
+    delta = zp.GetDelta()
 
     # Input
     keys = pygame.key.get_pressed()
 
-    if keys[pygame.K_e] and (zp.ThingGetVelocityY(bird) >= 0 or zp.ThingGetPosY(bird) == 5):
-        zp.ThingSetVelocityY(bird, -2.5)
+    # Jump
+    if keys[pygame.K_e] and zp.ThingGetVelocityY(bird) >= 0:
+        zp.ThingSetVelocityY(bird, -20)
 
-    zp.ThingSetPosZ(bird, 5)
-
-    # Z3dPy Physics System
+    # Physics
     zp.HandlePhysicsFloor([bird], 5)
 
     # Smoothly following the bird
     # CamChase(camera, targetLocation, speed)
-    zp.CamChase(myCamera, [zp.ThingGetPosX(bird), zp.ThingGetPosY(bird), -3], 0.1)
+    zp.CamChase(myCamera, [zp.ThingGetPosX(bird), zp.ThingGetPosY(bird), -3], 10)
 
     zp.CamSetTargetDir(myCamera, [0, 0, 1])
 
     zp.SetInternalCam(myCamera)
 
-    # Tilting Bird
-    rotation = min(zp.ThingGetVelocityY(bird) * 15, 40)
+    # Bird Rotation
+    rotation = zp.ThingGetVelocityY(bird) * 2
+    rotation = min(rotation, 40)
     if zp.ThingGetPosY(bird) == 5:
         zp.ThingSetRot(bird, [0, 0, 0])
     else:
         zp.ThingSetRot(bird, [0, 0, rotation])
 
     # Moving Pipe Pairs
-    zp.ThingSubPos(pipe, [0.2, 0, 0])
+    zp.ThingSubPos(pipe, [5 * delta, 0, 0])
     if zp.ThingGetPosX(pipe) <= -8:
         SpawnPipePair(rand.random() * 6 + 1)
 
+    # Render 3D
     for tri in zp.Raster():
-        normal = zp.TriGetNormal(tri)
-        zp.PgDrawTriS(tri, (normal[2] + normal[1]) / 2, screen, pygame)
+        zp.PgDrawTriFLB(tri, screen, pygame)
 
     # Update display
     pygame.display.flip()
 
+    fps = int(1 / (time.time() - currentTime))
+
+    # Setting the caption is much faster than printing
+    pygame.display.set_caption(str(fps) + " FPS")
+    currentTime = time.time()
     
