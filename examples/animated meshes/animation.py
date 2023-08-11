9b6ab7172f39ea0e937b56ca3d80b02f6adbd2fb
Zack <115175938+ZackWilde27@users.noreply.github.com>
2023-08-11 17:25:10 -0600

    Update animation.py

--- examples/animated meshes/animation.py
+++ examples/animated meshes/animation.py
@@ -11,15 +11,15 @@ zp.screenSize = (1680, 720)
 #zp.FindHowVars(65, 9/21)
 zp.SetHowVars(0.6727219660070214, 1.5696847505584837)
 
-# z3dpy.Camera(x, y, z)
-myCamera = zp.Cam(-10, -2, 3)
+# z3dpy.Camera(vPos)
+myCamera = zp.Cam([-10, -2, 3])
 
 zp.CamSetTargetDir(myCamera, [1, 0, 0])
 
 zp.SetInternalCam(myCamera)
 
-# z3dpy.LoadAnimMesh(filename, x, y, z)
-# z3dpy.Thing(meshList, x, y, z)
+# z3dpy.LoadAnimMesh(filename, *vPos, *VScale)
+# z3dpy.Thing(meshList, vPos)
 anim = zp.LoadAniMesh("mesh/anim/anim.obj")
 anim2 = zp.LoadAniMesh("mesh/char2/char2.obj")
 
@@ -28,10 +28,10 @@ zp.MeshSetColour(anim, [0, 255, 0])
 zp.MeshSetColour(anim2, [255, 0, 0])
 
 
-char1 = zp.Thing([anim], 0, 0, 3)
-char2 = zp.Thing([anim2], 0, 0, 3)
+char1 = zp.Thing([anim], [0, 0, 3])
+char2 = zp.Thing([anim2], [0, 0, 3])
 
-plane = zp.Thing([zp.LoadMesh("z3dpy/mesh/plane.obj")], 0, 0, 3)
+plane = zp.Thing([zp.LoadMesh("z3dpy/mesh/plane.obj")], [0, 0, 3])
 
 zp.AddThing(char1, 3)
 zp.AddThing(char2)
@@ -49,7 +49,7 @@ while True:
     
     for tri in zp.Raster():
         nrm = zp.TriGetNormal(tri)
-        shade = (-nrm[0] + nrm[1]) * 0.5
+        shade = (nrm[0] + -nrm[1]) * 0.5
         zp.PgDrawTriS(tri, shade, screen, pygame)
 
     # Play animation
